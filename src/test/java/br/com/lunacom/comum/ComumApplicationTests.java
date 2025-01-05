9de35de7408fddf858f18a0f491595d24d9d47eb
alexpereira2014 <alexpereira2004@gmail.com>
2025-01-05 19:38:55 -0300

    Primeiro commit

---
+++ src/test/java/br/com/lunacom/comum/ComumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lunacom.comum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
